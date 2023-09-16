ae4829525e87180e1a42eab8997b097cd0006db5
mohammadrezapourreza <m1378.prz@gmail.com>
2023-09-15 21:39:49 -0400

    adding all of the pages

--- home.py
+++ home.py
@@ -1,9 +1,13 @@
-from PIL import Image
-from pathlib import Path
-
 import requests
 import json
 import streamlit as st
+import time
+import pandas as pd
+
+from PIL import Image
+from pathlib import Path
+from clear_results import with_clear_container
+
 
 LOGO_PATH = Path(__file__).parent / "images" / "logo.png"
 
@@ -36,10 +40,55 @@ def add_database_connection(connection_data):
             return None
     except requests.exceptions.RequestException:
         return None
+    
+def answer_question(db_connection_id, question):
+    api_url = f'{HOST}/api/v1/question'
+    request_body = {
+        "db_connection_id": db_connection_id,
+        "question": question
+    }
+    try:
+        response = requests.post(api_url, json=request_body)
+        if response.status_code == 200:
+            response_data = response.json()
+            return response_data
+        else:
+            st.warning(f"Could not answer question because {response.text}.")
+            return {}
+    except requests.exceptions.RequestException as e:
+        st.error(f"Connection failed due to {e}.")
+        return {}
 
 def load_image(LOGO_PATH):
     img =  Image.open(LOGO_PATH)
     return img
+
+def type_text(text, answer_container):
+    text = text.strip()
+    answer_container =  st.empty()
+    for i in range(len(text) + 1):
+        answer_container.markdown(text[0:i])
+        time.sleep(0.02)
+    st.divider()
+
+def json_to_dataframe(json_data):
+    if json_data is None:
+        return pd.DataFrame()
+    columns = json_data.get("columns", [])
+    rows = json_data.get("rows", [])
+    df = pd.DataFrame(rows, columns=columns)
+    return df
+
+def test_connection():
+    try:
+        url = HOST + '/api/v1/heartbeat'
+        response = requests.get(url)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+    except requests.exceptions.ConnectionError:
+        return False
 
 st.set_page_config(
     page_title="Dataherald",
@@ -52,7 +101,7 @@ st.sidebar.write("Ask questions about your data.")
 st.sidebar.subheader("Connect to the engine")
 HOST = st.sidebar.text_input("Engine URI", value="http://localhost")
 st.session_state["HOST"] = HOST
-if st.sidebar.button("Connect to engine"):
+if st.sidebar.button("Connect"):
     try:
         url = HOST + '/api/v1/heartbeat'
         response = requests.get(url)
@@ -62,8 +111,13 @@ if st.sidebar.button("Connect to engine"):
         st.sidebar.error("Connection failed.")
 
 # Setup main page
-st.title("Dataherald engine")
-st.info("Please connect to the engine on the left sidebar.")
+st.title("Dataherald Engine")
+
+if not test_connection():
+    st.error("Could not connect to engine. Please connect to the engine on the left sidebar.")  # noqa: E501
+    st.stop()
+else:
+    st.info("Connect to a database and ask your question.")
 
 current_database = ""
 with st.form("database_connection"):
@@ -73,4 +127,36 @@ with st.form("database_connection"):
     connect = st.form_submit_button("Connect to database")
     if connect:
         st.success(f"Connected to {database_connection}.")
-        current_database = database_connection
+        st.session_state["database_connection_id"] = database_connections[database_connection]  # noqa: E501
+
+with st.form(key="form"):
+    user_input = st.text_input("Ask your question")
+    submit_clicked = st.form_submit_button("Submit Question")
+
+output_container = st.empty()
+if with_clear_container(submit_clicked):
+    output_container = output_container.container()
+    output_container.chat_message("user").write(user_input)
+    answer_container = output_container.chat_message("assistant")
+    introduction = ":wave: Hello. Please, give me a few moments and I'll be back with your answer."  # noqa: E501
+    type_text(introduction, answer_container)
+    with st.spinner("Thinking..."):
+        try:
+            answer = answer_question(
+                st.session_state["database_connection_id"],
+                user_input)  
+        except KeyError:
+            st.error("Please connect to a database first.")
+            st.stop()
+    try:
+        sql_query = f"📝 Generated SQL Query: {answer['sql_query']}"
+        type_text(sql_query, answer_container)
+        confidence = f"📊 Confidence: {answer['confidence_score']}"
+        type_text(confidence, answer_container)
+        nl_answer = f"🤔 Agent response: {answer['nl_response']}"
+        type_text(nl_answer, answer_container)
+        results_from_db = json_to_dataframe(answer["sql_query_result"])
+        answer_container.dataframe(results_from_db)
+    except KeyError:
+        st.error("Please connect to a correct database first.")
+        st.stop()
--- pages/2_💾_ContextStore.py
+++ pages/2_💾_ContextStore.py
@@ -1,10 +1,12 @@
 import streamlit as st
+import pandas as pd
 import requests
 import json
+import sys
 
 
 def get_all_database_connections():
-    api_url = HOST + '/api/v1/database-connections'
+    api_url = f'{HOST}/api/v1/database-connections'
     try:
         response = requests.get(api_url)
         if response.status_code == 200:
@@ -20,13 +22,15 @@ def get_all_database_connections():
         st.error("Connection failed.")
         return {}
 
-def add_golden_records(golden_records_data):
-    api_url = HOST + '/api/v1/golden-records'
+def add_golden_records(data):
+    api_url = f'{HOST}/api/v1/golden-records'
 
     try:
-        response = requests.post(api_url, json=golden_records_data)
-        if response.status_code == 200:
-            st.success("Golden records added.")
+        response = requests.post(api_url, json=data)
+        
+        if response.status_code == 201:
+            st.success("Golden record(s) added successfully.")
+            st.session_state["connection_id"] = None
             return True
         else:
             st.warning(f"Could not add golden records because {response.text}.")
@@ -35,6 +39,38 @@ def add_golden_records(golden_records_data):
         st.error("Connection failed.")
         return False
 
+def get_golden_records(page=1, limit=sys.maxsize):
+    api_url = f'{HOST}/api/v1/golden-records'
+    params = {
+        'page': page,
+        'limit': limit
+    }
+    try:
+        response = requests.get(api_url, params=params)
+        if response.status_code == 200:
+            golden_records = response.json()
+            return golden_records
+        else:
+            st.warning("Could not get golden records.")
+            return []
+    except requests.exceptions.RequestException as e:
+        st.error(f"Connection failed due to {e}.")
+        return []
+
+def delete_golden_record(golden_record_id):
+    api_url = f'{HOST}/api/v1/golden-records/{golden_record_id}'
+
+    try:
+        response = requests.delete(api_url)
+        if response.status_code == 200:
+            st.success("Golden record deleted successfully.")
+            return True
+        else:
+            st.warning(f"Could not delete golden record because {response.text}.")
+            return False
+    except requests.exceptions.RequestException as e:
+        st.error(f"Connection failed due to {e}.")
+        return False
 
 st.set_page_config(
     page_title="Dataherald",
@@ -47,6 +83,7 @@ HOST = st.session_state["HOST"]
 st.title("Golden Records")
 
 with st.form("Databases"):
+    st.info("Select a database connection to add or delete golden records.")
     db_connection_id = None
     st.subheader("Choose database connection")
     database_connections = get_all_database_connections()
@@ -55,44 +92,75 @@ with st.form("Databases"):
         options=list(database_connections.keys()))
     if st.form_submit_button("Select"):
         st.success(f"Connected to {db_connection}.")
-        db_connection_id = database_connections[db_connection]
-    
-
-if not db_connection_id:
-    st.warning("Please select a database connection.")
+        st.session_state["connection_id"] = database_connections[db_connection]
+        connection_id = database_connections[db_connection]
 
 with st.form("Golden records"):
     add_or_upload = st.radio(
         "Add or upload golden records",
         ("Add", "Upload"))
     add_column, upload_column = st.columns(2)
-    add_column.subheader("Add golden records")
+    add_column.subheader("Add a golden record")
     question = add_column.text_input("Question")
     sql_query = add_column.text_input("SQL query")
-    added_golden_records_data = [
-    {
-        "db_connection_id": str(db_connection_id),
-        "question": question,
-        "sql_query": sql_query
-    }
-    ]
-
     upload_column.subheader("Upload golden records")
     uploaded_file = upload_column.file_uploader(
-        "Upload jsonl file",
+        "Upload jsonl file (JSONL should contain question and sql_query fields))",
         type=["jsonl"])
-    uploaded_golden_records_data = []
-    if uploaded_file:
-        uploaded_golden_records_data = uploaded_file.readlines()
-        uploaded_golden_records_data = [
-            json.loads(line)
-            for line in uploaded_golden_records_data
-        ]
     if st.form_submit_button("Add"):
         if add_or_upload == "Add":
-            with st.spinner("Adding golden records..."):
-                add_golden_records(added_golden_records_data)
+            with st.spinner("Adding golden record..."):
+                data = {
+                    "db_connection_id": st.session_state["connection_id"],
+                    "question": question,
+                    "sql_query": sql_query
+                }
+                add_golden_records([data])
         else:
             with st.spinner("Uploading golden records..."):
-                add_golden_records(uploaded_golden_records_data)
+                if uploaded_file is not None:
+                    uploaded_data = []
+                    for line in uploaded_file:
+                        try:
+                            line_data = json.loads(line.decode("utf-8").strip())  
+                            if "question" in line_data and "sql_query" in line_data:
+                                uploaded_data.append({
+                                    "db_connection_id": st.session_state["connection_id"],  # noqa: E501
+                                    "question": line_data["question"],
+                                    "sql_query": line_data["sql_query"]
+                                })
+                            else:
+                                st.warning("Uploaded file contains incomplete data.")
+                        except json.JSONDecodeError as e:
+                            st.warning(
+                                f"Invalid JSON format in the uploaded file due to {e}."
+                                )
+                            
+                    add_golden_records(uploaded_data)
 
+with st.form("View golden records"):
+    st.subheader("View golden records")
+    page = st.number_input("Page", value=1)
+    limit = st.slider("Limit", min_value=1, max_value=20, value=10)
+    if st.form_submit_button("View"):
+        with st.spinner("Loading golden records..."):
+            golden_records = get_golden_records()
+            df = pd.DataFrame(golden_records)
+            df = df[df['db_connection_id'] == st.session_state["connection_id"]]
+            df = df.iloc[(page-1)*limit:page*limit]
+            df.drop(columns=["db_connection_id"], inplace=True)
+            df.reset_index(drop=True, inplace=True)
+            if golden_records:
+                st.dataframe(df)
+            else:
+                st.warning("No golden records found.")
+
+with st.form("Delete golden record"):
+    st.subheader("Delete golden record")
+    golden_record_id = st.text_input("Golden record ID")
+    if st.form_submit_button("Delete"):
+        if golden_record_id:
+            with st.spinner("Deleting golden record..."):
+                delete_golden_record(golden_record_id)
+        else:
+            st.warning("Please provide a golden record ID.")
